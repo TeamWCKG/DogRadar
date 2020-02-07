7c93f0458bc15f6aa78884792d9b66c68d818a6f
Kiran Junnarkar <kjunnarkar@yahoo.com>
2020-02-07 13:18:42 -0800

    Updated: App, DogsList, Dogs

--- client/src/components/Dogs.js
+++ client/src/components/Dogs.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import axios from "axios";
+import DogContext from '../Context/DogContext'
 
  const Dogs=()=>{
+    const { dogs } = useContext(DogContext);
+    
     return (
         <div>
 
