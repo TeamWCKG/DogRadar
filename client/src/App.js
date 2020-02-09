c2083d2da2b356e3a85bbc9638acc8a7f0fc56e2
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-02-08 19:01:54 -0800

    added res.data to the api (#3)
    Co-authored-by: Carlos Eduardo Turcios <52684059+CarlosETurcios@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import {DogContext} from "../src/Context/DogContext";
+import { DogContext } from "../src/Context/DogContext";
 import axios from 'axios';
 
 
 function App() {
-  const [dogs, setDogs]=useState({})
+  const [dogs, setDogs] = useState({})
 
   axios
+
   .get("http://localhost:5000/dogadar")
   .then( res=>{
     console.log("response", res.data.dogs)
   })  
   .catch(error => console.log("Did not get API", error));
 
-
   return (
     <DogContext.Provider value={dogs}>
-    <div className="App">
-      <h1> DOG RADAR</h1>
-    </div>
+      <div className="App">
+        <h1> DOG RADAR</h1>
+      </div>
     </DogContext.Provider>
   );
 }
